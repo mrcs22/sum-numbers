dc415b385bc2f205c445cf623438e3df803b6f27
Marcus Moraes <marcus.moraes@devio.com.br>
2023-11-26 13:17:24 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Soma de Múltiplos de 3 e 5
+
+#### Este projeto consiste na implementação de uma função que recebe um número inteiro positivo e retorna a soma de todos os valores inteiros divisíveis por 3 ou 5 que são inferiores ao número fornecido.
+
+### Exemplos:
+- Se a função receber o inteiro 10, ela deve retornar 23, resultante do somatório dos números 3, 5, 6 e 9 que são menores que 10.
+- Se a função receber o inteiro 11, ela deve retornar 33, resultante do somatório dos números 3, 5, 6, 9 e 10 que são menores que 11.
+
+### Utilização:
+Você pode usar esta aplicação de duas maneiras:
+
+#### 1. Modo Web:
+A aplicação está disponível online para uso direto <a href="https://mrcs22.github.io/sum-numbers/">neste link</a>
+
+#### 2. Modo Local (Node.js):
+Clone este repositório para sua máquina
+```bash
+git clone git@github.com:mrcs22/sum-numbers.git
+```
+Acesse o diretório do projeto
+  ```bash
+cd numbers-sum
+```
+Execute o script.js com Node.js, fornecendo o número desejado como argumento:
+```bash
+node script.js 42 
+```
+
+Certifique-se de ter o Node.js instalado em sua máquina.
---
+++ script.js
@@ -0,0 +1,80 @@
+function getArithmeticProgression(multiple, number) {
+    const multiplesAmount = Math.floor((number -1) / multiple);
+    
+    const result = multiple * (multiplesAmount * (multiplesAmount + 1)) / 2;
+    return result;
+}
+
+function sumMultiplesOfThreeAndFiveUpToValue(value){
+    const multiplesOfFiveSum = getArithmeticProgression(5, value);
+    const multiplesOfThreeSum = getArithmeticProgression(3, value);
+    const multiplesOfFifteenSum = getArithmeticProgression(15, value);
+
+    return multiplesOfFiveSum + multiplesOfThreeSum - multiplesOfFifteenSum;
+}
+
+function calculateFromWeb() {
+    const inputNumber = document.getElementById("inputNumber").value;
+    const output = document.getElementById("output");
+
+    if (inputNumber.trim() === "") {
+        alert("É preciso digitar um número");
+        output.value = '0'
+        return;
+    }
+
+    const parsedInput = parseFloat(inputNumber);
+
+    if (isNaN(parsedInput) || !Number.isInteger(parsedInput)) {
+        alert("Digite um número inteiro válido");
+        output.value = '0'
+        return;
+    }
+
+    if (parsedInput < 0) {
+        alert("Digite um inteiro positivo.");
+        output.value = '0'
+        return;
+    }
+
+    output.value = sumMultiplesOfThreeAndFiveUpToValue(parsedInput)
+}
+
+ 
+
+function calculateFromTerminal(inputNumber) {
+    const output = {};
+
+    if (inputNumber.trim() === "") {
+        console.log("É preciso digitar um número");
+        output.value = '0';
+        return output;
+    }
+
+    const parsedInput = parseFloat(inputNumber);
+
+    if (isNaN(parsedInput) || !Number.isInteger(parsedInput)) {
+        console.log("Digite um número inteiro válido");
+        output.value = '0';
+        return output;
+    }
+
+    if (parsedInput < 0) {
+        console.log("Digite um inteiro positivo.");
+        output.value = '0';
+        return output;
+    }
+
+    output.value = sumMultiplesOfThreeAndFiveUpToValue(parsedInput)
+    return output;
+}
+
+
+if (process.argv.length < 3) {
+    console.log("Por favor, forneça um número como argumento.");
+} else {
+    const inputNumber = process.argv[2];
+
+    const result = calculateFromTerminal(inputNumber);
+    console.log("Resultado:", result.value);
+}
